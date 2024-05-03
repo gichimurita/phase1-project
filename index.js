76f0981fd6516c038479a073e9f9658175443198
Rita Gichimu <wangeci.gichimu@gmail.com>
2024-05-03 22:19:54 +0300

    add event listener

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const quoteText = document.querySelector('.quote-text');
+    const newQuoteBtn = document.querySelector('.new-quote-btn');
+
+    newQuoteBtn.addEventListener('click', getQuote);
