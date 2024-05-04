1b23bd5b5e41df94e41464b3a6dd1617a4f263ef
Rita Gichimu <wangeci.gichimu@gmail.com>
2024-05-04 10:45:05 +0300

    update index.js

--- index.js
+++ index.js
@@ -4,19 +4,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
     newQuoteBtn.addEventListener('click', getQuote);
 
-
-    async function getQuote() {
-        try {
-            const response = await fetch('https://quote-garden.onrender.com/api/v3/quotes/random');
-            const data = await response.json();
-            const quote = data.data[0];
-            quoteText.textContent = `"${quote.quoteText}" - ${quote.quoteAuthor}`;
-        } catch (error) {
-            console.error('Error fetching quote:', error);
-            quoteText.textContent = 'Failed to fetch quote. Please try again later.';
-        }
-    }
-
-    // Initial Quote
-    getQuote();
-});
